b69746ff40a289ae7ad26429cce3731a14cb6f21
MAbdurahman <mdbdrrhm@gmail.com>
2023-02-19 11:03:40 -0600

    Add styles need some adjustments

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* ============================================
+            preloader
+===============================================*/
+$(window).on('load', function () {
+  // makes sure that whole site is loaded
+  /*$('#preloader-gif, #preloader').fadeOut(3000, function () {});*/
+});
+/*=============================================
+          js-weather-app
+================================================*/
+window.onload = function () {
+
+
+
+
+}//end of window.onload function
